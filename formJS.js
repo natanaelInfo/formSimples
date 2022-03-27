3d43e07bd639bd2ebdda577ad381b87786a5fcfb
natanaelInfo <101744603+natanaelInfo@users.noreply.github.com>
2022-03-27 18:05:22 -0300

    Form Simples
    Form Simples

---
+++ formJS.js
@@ -0,0 +1,13 @@
+
+
+
+nome.addEventListener('keyup', function(event){
+   
+    let nome = document.forms[0].querySelector('#nome');
+
+    let label1 =  document.forms[0].querySelector('#labe1');
+    
+    console.log(label1);
+
+ 
+}); 
